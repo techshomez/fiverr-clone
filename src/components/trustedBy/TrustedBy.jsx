065f8162069e112418bc3bfb37c807dea6223ff0
techshomez <danieltopeg>
2023-02-14 23:35:22 +0100

    Trusted component completed

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,33 @@
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <span>Trusted by:</span>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook.31d5f92.png"
+          alt=""
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google.517da09.png"
+          alt=""
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix.e3ad953.png"
+          alt=""
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg.8b7310b.png"
+          alt=""
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal.ec56157.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Feature";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
     </div>
   );
 };
