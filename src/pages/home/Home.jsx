b28fd4cac61e2042d9b787640c22ad2f8fa175e8
techshomez <danieltopeg@gmail.com>
2023-02-16 22:58:07 +0100

    working on the video and information components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 import { cards } from "../../data";
 import CategoryCard from "../../components/catCard/catCard";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -15,6 +16,46 @@ const Home = () => {
           return <CategoryCard item={card} key={card.id} />;
         })}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
