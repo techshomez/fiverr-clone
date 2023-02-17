4baee25df9ada7d5c9975f6e6771817ba1446771
techshomez <danieltopeg@gmail.com>
2023-02-17 01:01:23 +0100

    feature business component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -56,6 +56,44 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>Fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits,
+              dedicated to businesses
+            </p>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <i>
+                <FaRegCheckCircle />
+              </i>
+              Connect to freelancers with proven business experience
+            </div>
+            <button>Explore Fiverr Business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
