d1ad54b0b8f3c071dc1edb50fd8f74ab963a41e9
Daniel Tope <danieltopeg@gmail.com>
2023-02-22 21:00:03 +0100

    footer bugs fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,60 +20,49 @@ const Footer = () => {
             <span>Sitemap</span>
           </div>
           <div className="item">
-            <h2>Categories</h2>
-            <span>Graphics & Design</span>
-            <span>Digital Marketing</span>
-            <span>Writing & Translation</span>
-            <span>Video & Animation</span>
-            <span>Music & Audio</span>
-            <span>Programming & Tech</span>
-            <span>Data</span>
-            <span>Business</span>
-            <span>Lifestyle</span>
-            <span>Photography</span>
-            <span>Sitemap</span>
+            <h2>About</h2>
+            <span>Careers</span>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy & Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
           </div>
           <div className="item">
-            <h2>Categories</h2>
-            <span>Graphics & Design</span>
-            <span>Digital Marketing</span>
-            <span>Writing & Translation</span>
-            <span>Video & Animation</span>
-            <span>Music & Audio</span>
-            <span>Programming & Tech</span>
-            <span>Data</span>
-            <span>Business</span>
-            <span>Lifestyle</span>
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Fiverr</span>
             <span>Photography</span>
-            <span>Sitemap</span>
+            <span>Buying on Fiverr</span>
           </div>
           <div className="item">
-            <h2>Categories</h2>
-            <span>Graphics & Design</span>
-            <span>Digital Marketing</span>
-            <span>Writing & Translation</span>
-            <span>Video & Animation</span>
-            <span>Music & Audio</span>
-            <span>Programming & Tech</span>
-            <span>Data</span>
-            <span>Business</span>
-            <span>Lifestyle</span>
-            <span>Photography</span>
-            <span>Sitemap</span>
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
           </div>
           <div className="item">
-            <h2>Categories</h2>
-            <span>Graphics & Design</span>
-            <span>Digital Marketing</span>
-            <span>Writing & Translation</span>
-            <span>Video & Animation</span>
-            <span>Music & Audio</span>
-            <span>Programming & Tech</span>
-            <span>Data</span>
-            <span>Business</span>
-            <span>Lifestyle</span>
-            <span>Photography</span>
-            <span>Sitemap</span>
+            <h2>More From Fiverr</h2>
+            <span>Fiverr Business</span>
+            <span>Fiverr Pro</span>
+            <span>Fiverr Logo Maker</span>
+            <span>Fivers Guides</span>
+            <span>Get Inspired</span>
+            <span>Fiverr Select</span>
+            <span>Clear Voice</span>
+            <span>Fiverr Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
           </div>
         </div>
         <hr />
