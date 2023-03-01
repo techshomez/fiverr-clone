e70eac9f20ba81b1c234568705bebc2e38bf845e
Daniel Tope <danieltopeg@gmail.com>
2023-03-01 16:20:53 +0100

    updating message component

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,46 +1,8 @@
-import { Link } from "react-router-dom";
+import React from "react";
 import "./Message.scss";
 
 const Message = () => {
-  const CurrentUser = {
-    id: 1,
-    username: "Tope Daniel",
-    isSeller: true,
-  };
-
-  return (
-    <div className="orders">
-      <div className="container">
-        <div className="title">
-          <h1>Order Page</h1>
-        </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>{CurrentUser?.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Contact</th>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig 1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  );
+  return <div className="message">Message</div>;
 };
 
 export default Message;
