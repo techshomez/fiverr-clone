ed09c96f7c45b3aed03c39c47d1610d2ea29ddca
Daniel Tope <danieltopeg@gmail.com>
2023-03-01 16:12:28 +0100

    order and gig components

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -7,14 +7,93 @@ const MyGigs = () => {
       <div className="container">
         <div className="title">
           <h1>Gigs</h1>
-          <Link to="/add">Add New Gig</Link>
+          <Link to="/add">
+            <button>Add New Gig</button>
+          </Link>
         </div>
         <table>
-          <tr>Image</tr>
-          <tr>Title</tr>
-          <tr>Price</tr>
-          <tr>Orders</tr>
-          <tr>Action</tr>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
         </table>
       </div>
     </div>
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,106 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./Orders.scss";
 
 const Orders = () => {
-  return <div className="orders">Orders</div>;
+  const CurrentUser = {
+    id: 1,
+    username: "Tope Daniel",
+    isSeller: true,
+  };
+
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Order Page</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{CurrentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
