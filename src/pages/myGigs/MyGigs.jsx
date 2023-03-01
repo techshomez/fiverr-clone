8ad3492dd082a60323e39ca15baf632211969882
Daniel Tope <danieltopeg@gmail.com>
2023-03-01 15:03:34 +0100

    my gig component

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./MyGigs.scss";
 
 const MyGigs = () => {
-  return <div className="mygigs">MyGigs</div>;
+  return (
+    <div className="mygigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add">Add New Gig</Link>
+        </div>
+        <table>
+          <tr>Image</tr>
+          <tr>Title</tr>
+          <tr>Price</tr>
+          <tr>Orders</tr>
+          <tr>Action</tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyGigs;
