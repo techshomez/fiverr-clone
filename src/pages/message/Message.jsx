a237166890a3ab16845d7a659aeffc1b8f60ac96
Daniel Tope <danieltopeg@gmail.com>
2023-03-01 17:30:59 +0100

    message body component

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -8,7 +8,80 @@ const Message = () => {
         <span className="breadcrumbs">
           <Link to="/messages">Messages</Link> > Tope Daniel
         </span>
-        <div className="messages"></div>
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://avatars.githubusercontent.com/u/46303140?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              non nobis, consectetur, odit nemo error repellat architecto
+              voluptatibus quam dolores rerum corrupti harum molestiae iste ea
+              mollitia facere nisi ex.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://avatars.githubusercontent.com/u/46303140?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              non nobis, consectetur, odit nemo error repellat architecto
+              voluptatibus quam dolores rerum corrupti harum molestiae iste ea
+              mollitia facere nisi ex.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://avatars.githubusercontent.com/u/46303140?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              non nobis, consectetur, odit nemo error repellat architecto
+              voluptatibus quam dolores rerum corrupti harum molestiae iste ea
+              mollitia facere nisi ex.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </div>
         <div className="write">
           <textarea
             name=""
@@ -17,6 +90,7 @@ const Message = () => {
             cols="30"
             rows="10"
           ></textarea>
+          <button>Send</button>
         </div>
       </div>
     </div>
