b1d24992adeced846751bb5ed267391269bf0dff
Daniel Tope <danieltopeg@gmail.com>
2023-03-01 17:04:01 +0100

    Message/chat body

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./Message.scss";
 
 const Message = () => {
-  return <div className="message">Message</div>;
+  return (
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link> > Tope Daniel
+        </span>
+        <div className="messages"></div>
+        <div className="write">
+          <textarea
+            name=""
+            placeholder="write a message"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
